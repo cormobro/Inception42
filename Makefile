9ab218d1f38e3e4b1edd737d284c07df9a18977d
Félix Bonaert <fbonaert@Felixs-MacBook-Air.local>
2024-10-18 15:37:31 +0200

    Probably finished, last tests on vm

--- Makefile
+++ Makefile
@@ -1,1 +1,25 @@
-docker system prune -a --volumes
+all: srcs/.env build
+
+build:
+	mkdir -p /home/febonaer/data/mariadb
+	mkdir -p /home/febonaer/data/wordpress
+	@docker-compose --file=srcs/docker-compose.yml up -d
+
+fclean:
+	@docker stop $(shell docker ps -qa)
+	@docker rm $(shell docker ps -qa)
+	@docker image rm -f $(shell docker images -qa)
+	@docker volume rm $(shell docker volume ls -q)
+	@docker network rm srcs_inception
+	sudo rm -rf /home/febonaer/data/mariadb
+	sudo rm -rf /home/febonaer/data/wordpress
+
+prune:
+	@docker system prune
+
+srcs/.env:
+	@echo "Missing .env file" && exit 1
+
+re: fclean build
+
+.PHONY: all build fclean re prune
--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-# Check if the database directory exists
-#rm -rf /var/lib/mysql/$SQL_DATABASE
 if [ -d "/var/lib/mysql/$SQL_DATABASE" ]; then
-    # If the directory exists, display a message indicating that the database already exists
     echo "Database already exists"
-    #echo "Rebuilding new database"
-    #rm -rf /var/lib/mysql/$MYSQL_DATABASE
 else
-    # If the directory does not exist, perform the following actions:
-
-    # Upgrade MySQL (MariaDB) to the latest version
     mysql_upgrade --force
-
-    # Install the MariaDB system tables and data files
     mysql_install_db
-
-    # Start the MariaDB service
     service mariadb start
-    # Run the script to secure the MariaDB installation, providing answers to prompts
-echo --------------------------------
     mysql_secure_installation << EOF
-
 	n
 	Y
 	${SQL_ROOT_PASSWORD}
@@ -31,27 +16,13 @@ echo --------------------------------
 	Y
 	Y
 EOF
-echo ----------------------------------
-    # Access the MariaDB server and execute SQL commands to create database and user
-    #mariadb << EOF
-    #CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}`;
-    #CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
-    #GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
-    #FLUSH PRIVILEGES;
-#EOF
-
     mariadb << EOF
     CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
     CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
     FLUSH PRIVILEGES;
 EOF
-
-
-    # Stop the MariaDB service
     service mariadb stop
 fi
-
-# Execute mysqld with bind address set to accept connections from any IP address
 exec mysqld --bind-address=0.0.0.0
 
