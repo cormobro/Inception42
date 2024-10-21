a1ed55e20afe2a51917dcbe049dd07185a822432
Félix Bonaert <fbonaert@Felixs-MacBook-Air.local>
2024-10-21 19:10:52 +0200

    Finally fixed

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,28 +1,31 @@
-#!/bin/bash
+# !/bin/bash
 
-if [ -d "/var/lib/mysql/$SQL_DATABASE" ]; then
-    echo "Database already exists"
-else
-    mysql_upgrade --force
-    mysql_install_db
-    service mariadb start
-    mysql_secure_installation << EOF
-	n
-	Y
-	${SQL_ROOT_PASSWORD}
-	${SQL_ROOT_PASSWORD}
-	Y
-	n
-	Y
-	Y
-EOF
+# if [ -d "/var/lib/mysql/$SQL_DATABASE" ]; then
+#     echo "Database already exists"
+# else
+#     mysql_upgrade --force
+#     mysql_install_db
+#     service mariadb start
+#     mysql_secure_installation << EOF
+# 	n
+# 	Y
+# 	${SQL_ROOT_PASSWORD}
+# 	${SQL_ROOT_PASSWORD}
+# 	Y
+# 	n
+# 	Y
+# 	Y
+# EOF
+service mariadb start
     mariadb << EOF
     CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
     CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+    ALTER USER '$SQL_ROOT'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
     FLUSH PRIVILEGES;
+    exit
 EOF
     service mariadb stop
-fi
+#fi
 exec mysqld --bind-address=0.0.0.0
 
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g" /etc/php/7.4/fpm/pool.d/www.conf
-ls -la
 rm -rf *
 #if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 wp core download --allow-root
-wp config create --allow-root --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_ROOT_PASSWORD} --dbhost=mariadb:3306 --dbcharset="utf8" --dbcollate="utf8_general_ci" --path="/var/www/html/wordpress"
+wp config create --allow-root --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --dbhost=mariadb:3306 --dbcharset="utf8" --dbcollate="utf8_general_ci" --path="/var/www/html/wordpress"
 echo ------------------------------------------
-wp core install --url=${WORDPRESS_URL} --title=${WORDPRESS_URL} --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_PASSWORD} --admin_email=${WORDPRESS_EMAIL} --allow-root
+wp core install --url=${WORDPRESS_URL} --title=${WORDPRESS_URL} --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_PASSWORD} --admin_email=${WORDPRESS_MAIL} --allow-root
 echo ------------------------------------------
 wp user create ${WORDPRESS_USER} ${WORDPRESS_USER_MAIL} --role=${WORDPRESS_USER_ROLE} --user_pass=${WORDPRESS_USER_PASSWORD} --allow-root
 wp plugin update --all --allow-root
